85c275898e6d287c2e3daa5b62ff9f41bccb835f
Daniel Lanza Garcia <daniel.lanza@cern.ch>
2016-10-07 16:02:32 +0200

    Perform the evaluation in threads

--- src/main/java/ec/steadystate/bloatcontrol/BloatControlSteadyStateEvaluator.java
+++ src/main/java/ec/steadystate/bloatcontrol/BloatControlSteadyStateEvaluator.java
@@ -1,6 +1,9 @@
 package ec.steadystate.bloatcontrol;
 
 import java.util.LinkedList;
+import java.util.NoSuchElementException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import ec.EvolutionState;
 import ec.Individual;
@@ -11,25 +14,37 @@ import ec.steadystate.SteadyStateEvaluator;
 public class BloatControlSteadyStateEvaluator extends SteadyStateEvaluator {
 
 	private static final long serialVersionUID = 1L;
-	
-    LinkedList<QueueIndividual> queue = new LinkedList<QueueIndividual>();
     
-    /** Holds the subpopulation currently being evaluated.  */ 
-    int subpopulationBeingEvaluated = -1;
+	private LinkedList<QueueIndividual> toBeEvaluatedIndividuals;
+	
+	private LinkedList<QueueIndividual> evaluatedIndividuals;
 
-    /** Our problem. */
-    SimpleProblemForm problem; 
+	public BloatControlSteadyStateEvaluator() {
+    	toBeEvaluatedIndividuals = new LinkedList<QueueIndividual>();
+    	evaluatedIndividuals = new LinkedList<QueueIndividual>();
+	}
 
 	@Override
-	public void prepareToEvaluate(EvolutionState state, int thread) {
-		// Init threads
-		problem = (SimpleProblemForm)p_problem.clone();
+	public void prepareToEvaluate(EvolutionState state, int thread) {		
+		int num_threads = 4;
+		
+		ExecutorService executor = Executors.newFixedThreadPool(num_threads);
+		for (int thread_num = 0; thread_num < num_threads; thread_num++) {
+			Runnable worker = new BloatControlSteadyStateEvaluatorThread(
+					thread_num,
+					state,
+					(SimpleProblemForm) p_problem.clone(),
+					toBeEvaluatedIndividuals,
+					evaluatedIndividuals);
+			executor.execute(worker);
+		}
 	}
 	
 	@Override
 	public void evaluateIndividual(EvolutionState state, Individual ind, int subpop) {
-		problem.evaluate(state, ind, subpop, 0);
-        queue.addLast(new QueueIndividual(ind, subpop));
+		synchronized (toBeEvaluatedIndividuals) {
+			toBeEvaluatedIndividuals.addLast(new QueueIndividual(ind, subpop));
+		}
 	}
 	
 	@Override
@@ -39,19 +54,25 @@ public class BloatControlSteadyStateEvaluator extends SteadyStateEvaluator {
 	
 	@Override
 	public Individual getNextEvaluatedIndividual() {
-        QueueIndividual qind = queue.removeFirst();
-        
-        if (qind == null) 
-        	return null;
-        
-        subpopulationBeingEvaluated = qind.subpop;
-        
-        return qind.ind;
+		synchronized (evaluatedIndividuals) {
+			try{
+				return evaluatedIndividuals.removeFirst().ind;
+			}catch(NoSuchElementException e){
+				return null;
+			}
+		}
 	}
 	
 	@Override
 	public int getSubpopulationOfEvaluatedIndividual() {
-		return subpopulationBeingEvaluated;
+		return 0;
+	}
+	
+	@Override
+	public boolean runComplete(EvolutionState state, Individual ind) {
+	    if (ind != null && ind.fitness.isIdealFitness())
+            return true;
+        return false;
 	}
 	
 }
---
+++ src/main/java/ec/steadystate/bloatcontrol/BloatControlSteadyStateEvaluatorThread.java
@@ -0,0 +1,65 @@
+package ec.steadystate.bloatcontrol;
+
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
+
+import ec.EvolutionState;
+import ec.simple.SimpleProblemForm;
+import ec.steadystate.QueueIndividual;
+
+public class BloatControlSteadyStateEvaluatorThread implements Runnable {
+	
+	private int thread_num;
+	private EvolutionState state;
+    private SimpleProblemForm problem;
+	private LinkedList<QueueIndividual> toBeEvaluatedIndividuals;
+	private LinkedList<QueueIndividual> evaluatedIndividuals;
+
+	private int emptyListCounter;
+	private int noEmptyListCounter;
+	
+	BloatControlSteadyStateEvaluatorThread(
+			int thread_num, 
+			EvolutionState state,
+			SimpleProblemForm problem,
+			LinkedList<QueueIndividual> toBeEvaluatedIndividuals,
+			LinkedList<QueueIndividual> evaluatedIndividuals){
+		this.thread_num = thread_num;
+		this.state = state;
+		this.problem = problem;
+		this.toBeEvaluatedIndividuals = toBeEvaluatedIndividuals;
+		this.evaluatedIndividuals = evaluatedIndividuals;
+		
+		emptyListCounter = 0;
+		noEmptyListCounter = 0;
+	}
+	
+	public void run() {
+		while(true){
+			try{
+				QueueIndividual ind = null;
+				synchronized (toBeEvaluatedIndividuals) {
+					if(toBeEvaluatedIndividuals.size() == 0){
+						emptyListCounter++;
+						
+						continue;
+					}else{
+						noEmptyListCounter++;
+					}
+					
+					ind = toBeEvaluatedIndividuals.removeFirst();
+				}
+				
+				if(noEmptyListCounter % 10000 == 0)
+					System.out.println(thread_num + ": " + noEmptyListCounter + " - " + emptyListCounter);
+				
+				problem.evaluate(state, ind.ind, 1, 0);
+				
+				synchronized (evaluatedIndividuals) {
+					evaluatedIndividuals.addLast(ind);
+				}
+			}catch(NoSuchElementException e){}
+		}
+	}
+
+}
