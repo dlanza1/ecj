c4ea957dabb051857a67c25990776bf479e2b3e2
Daniel Lanza <daniel.lanza@cern.ch>
2017-02-15 22:02:59 +0100

    Calculate best_of_run individual

--- src/main/java/ec/steadystate/SteadyStateStatistics.java
+++ src/main/java/ec/steadystate/SteadyStateStatistics.java
@@ -7,6 +7,7 @@
 package ec.steadystate;
 
 import ec.EvolutionState;
+import ec.Individual;
 import ec.gp.GPIndividual;
 import ec.simple.SimpleStatistics;
 
@@ -15,6 +16,23 @@ public class SteadyStateStatistics extends SimpleStatistics {
 	
 	@Override
 	public void postEvaluationStatistics(EvolutionState state) {
+        Individual[] best_i = new Individual[state.population.subpops.length];  // quiets compiler complaints
+        for(int x=0;x<state.population.subpops.length;x++)
+            {
+            best_i[x] = state.population.subpops[x].individuals[0];
+            for(int y=1;y<state.population.subpops[x].individuals.length;y++)
+                {
+                if (state.population.subpops[x].individuals[y] == null)
+                    {
+                    }
+                else if (best_i[x] == null || state.population.subpops[x].individuals[y].fitness.betterThan(best_i[x].fitness))
+                    best_i[x] = state.population.subpops[x].individuals[y];
+                }
+        
+            // now test to see if it's the new best_of_run
+            if (best_of_run[x]==null || best_i[x].fitness.betterThan(best_of_run[x].fitness))
+                best_of_run[x] = (Individual)(best_i[x].clone());
+            }
 	}
 
 	@Override
