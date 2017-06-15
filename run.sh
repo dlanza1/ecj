41883cd63fadb5d53ee004a521e9ba128679bac2
Daniel Lanza <daniel.lanza@cern.ch>
2017-06-15 18:45:38 +0200

    Reduce maximum number of groups to 128

--- run.sh
+++ run.sh
@@ -15,7 +15,7 @@ mkdir -p $HOME/out/
 
 problem_name="parity"
 java -cp $HOME/target/ecj-23-SNAPSHOT.jar ec.Evolve -file $HOME/src/main/java/ec/app/parity/parity-groups.params -p jobs=30 -p breed.groups=1 -p generations=50 -p stat.file=$HOME/out/${problem_name}.1.stat
-for groups in 512 256 128 64 32 16 8 4 2
+for groups in 128 64 32 16 8 4 2
 do
     java -cp $HOME/target/ecj-23-SNAPSHOT.jar ec.Evolve -file $HOME/src/main/java/ec/app/parity/parity-groups.params \
 			-p jobs=30 \
@@ -26,7 +26,7 @@ done
 
 problem_name="ant"
 java -cp $HOME/target/ecj-23-SNAPSHOT.jar ec.Evolve -file $HOME/src/main/java/ec/app/ant/ant-groups.params -p jobs=30 -p breed.groups=1 -p generations=50 -p stat.file=$HOME/out/${problem_name}.1.stat
-for groups in 512 256 128 64 32 16 8 4 2
+for groups in 128 64 32 16 8 4 2
 do
     java -cp $HOME/target/ecj-23-SNAPSHOT.jar ec.Evolve -file $HOME/src/main/java/ec/app/ant/ant-groups.params \
 			-p jobs=30 \
@@ -37,7 +37,7 @@ done
 
 problem_name="lawnmover"
 java -cp $HOME/target/ecj-23-SNAPSHOT.jar ec.Evolve -file $HOME/src/main/java/ec/app/lawnmower/noadf-groups.params -p jobs=30 -p breed.groups=1 -p generations=50 -p stat.file=$HOME/out/${problem_name}.1.stat
-for groups in 512 256 128 64 32 16 8 4 2
+for groups in 128 64 32 16 8 4 2
 do
     java -cp $HOME/target/ecj-23-SNAPSHOT.jar ec.Evolve -file $HOME/src/main/java/ec/app/lawnmower/noadf-groups.params \
                     	-p jobs=30 \
@@ -48,7 +48,7 @@ done
 
 problem_name="multiplexer"
 java -cp $HOME/target/ecj-23-SNAPSHOT.jar ec.Evolve -file $HOME/src/main/java/ec/app/multiplexer/11-groups.params -p jobs=30 -p breed.groups=1 -p generations=50 -p stat.file=$HOME/out/${problem_name}.1.stat
-for groups in 512 256 128 64 32 16 8 4 2
+for groups in 128 64 32 16 8 4 2
 do
     java -cp $HOME/target/ecj-23-SNAPSHOT.jar ec.Evolve -file $HOME/src/main/java/ec/app/multiplexer/11-groups.params \
                     	-p jobs=30 \
@@ -59,7 +59,7 @@ done
  
 problem_name="regression"
 java -cp $HOME/target/ecj-23-SNAPSHOT.jar ec.Evolve -file $HOME/src/main/java/ec/app/regression/sexticerc-groups.params -p jobs=30 -p breed.groups=1 -p generations=50 -p stat.file=$HOME/out/${problem_name}.1.stat
-for groups in 512 256 128 64 32 16 8 4 2
+for groups in 128 64 32 16 8 4 2
 do
     java -cp $HOME/target/ecj-23-SNAPSHOT.jar ec.Evolve -file $HOME/src/main/java/ec/app/regression/sexticerc-groups.params \
                     	-p jobs=30 \
@@ -70,7 +70,7 @@ done
 
 problem_name="royaltree"
 java -cp $HOME/target/ecj-23-SNAPSHOT.jar ec.Evolve -file $HOME/src/main/java/ec/app/royaltree/royaltree-groups.params -p jobs=30 -p breed.groups=1 -p generations=500 -p stat.file=$HOME/out/${problem_name}.1.stat
-for groups in 2048 1024 512 256 128 64 32 16 8 4 2
+for groups in 128 64 32 16 8 4 2
 do
     java -cp $HOME/target/ecj-23-SNAPSHOT.jar ec.Evolve -file $HOME/src/main/java/ec/app/royaltree/royaltree-groups.params \
                     	-p jobs=30 \
